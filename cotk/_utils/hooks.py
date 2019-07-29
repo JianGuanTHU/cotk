ec9f167b2b6fdbc4b5dfc0c35e0aa2d8948c00f0
hzhwcmhf <hzhwcmhf@gmail.com>
2019-07-29 22:03:47 +0800

    refactor hooks

--- cotk/_utils/hooks.py
+++ cotk/_utils/hooks.py
@@ -4,6 +4,8 @@ A utils providing callback hooks.
 
 from inspect import signature
 import pkg_resources
+import json
+import copy
 
 #pylint: disable=global-statement
 
@@ -14,6 +16,27 @@ def invoke_listener(method, *argv):
 	global hooks_listener
 	for listener in hooks_listener:
 		getattr(listener, method)(*argv)
+
+def compress_dict(dic):
+	res = {}
+
+	def peek_json_length(obj):
+		try:
+			return len(json.dumps(obj))
+		except TypeError:
+			return -1
+
+	for key, value in dic.items():
+		if peek_json_length(value) in range(0, 50):
+			res[key] = copy.deepcopy(value)
+		else:
+			if isinstance(value, list):
+				res[key] = "[...]"
+			elif isinstance(value, dict):
+				res[key] = "{...}"
+			else:
+				res[key] = "..."
+	return res
 
 def hook_dataloader(fn):
 	r'''decorator for dataloader.__init___'''
@@ -28,21 +51,31 @@ def hook_dataloader(fn):
 		return fn(*args, **kwargs)
 	return wrapped
 
-def hook_standard_metric(metric_name=""):
-	r'''decorator for dataloader.get_metric'''
-	def decorator(fn):
-		sign = signature(fn)
-		def wrapped(*args, **kwargs):
-			binded = sign.bind(*args, **kwargs)
-			binded.apply_defaults()
-			binded = dict(binded.arguments)
-			self = binded['self']
-			del binded['self']
-			invoke_listener("add_standard_metric", self, fn.__qualname__.split(".")[0], metric_name, binded)
-			print(kwargs)
-			return fn(*args, **kwargs)
-		return wrapped
-	return decorator
+def hook_metric(fn):
+	r'''decorator for metric.__init__'''
+	sign = signature(fn)
+	def wrapped(*args, **kwargs):
+		binded = sign.bind(*args, **kwargs)
+		binded.apply_defaults()
+		binded = dict(binded.arguments)
+		self = binded['self']
+		del binded['self']
+		invoke_listener("add_metric", self, fn.__qualname__.split(".")[0], binded)
+		return fn(*args, **kwargs)
+	return wrapped
+
+def hook_metric_close(fn):
+	r'''decorator for metric.close'''
+	sign = signature(fn)
+	def wrapped(*args, **kwargs):
+		binded = sign.bind(*args, **kwargs)
+		binded.apply_defaults()
+		binded = dict(binded.arguments)
+		self = binded['self']
+		return_dict = fn(*args, **kwargs)
+		invoke_listener("invoke_metric_close", self, return_dict)
+		return return_dict
+	return wrapped
 
 def hook_wordvec(fn):
 	r'''decorator for wordvec.__init__'''
@@ -59,45 +92,71 @@ def hook_wordvec(fn):
 
 class BaseHooksListener:
 	r'''An abstract class implement the basic hook listener'''
-	def add_dataloader(self, dataloader, args):
+	def add_dataloader(self, obj, dataloader, args):
 		pass
 
-	def add_standard_metric(self, dataloader, metric_type, args):
+	def add_metric(self, obj, metric, args):
 		pass
 
-	def add_wordvec(self, wordvec, args):
+	def invoke_metric_close(self, obj, return_dict):
+		pass
+
+	def add_wordvec(self, obj, wordvec, args):
 		pass
 
 class SimpleHooksListener(BaseHooksListener):
 	r'''An simple recorder'''
 	def __init__(self):
-		self.record = {
-			"cotk_version": pkg_resources.require("cotk")[0].version,
-			"dataloader": {},
-			"standard_metric": {},
-			"wordvec": {}
-		}
-		self.dataloader_id = {}
+		self.dataloader_set = {}
+		self.metric_set = {}
+		self.hash_set = {}
+		self.wordvec_set = {}
 
-	def close(self):
-		return self.record
+	def close(self, result_dic):
+		record = {
+			"cotk_version": pkg_resources.require("cotk")[0].version,
+			"dataloader": [],
+			"wordvec": []
+		}
+		record_dataloader_set = {}
+		for key, value in result_dic.items():
+			if "hashvalue" in key:
+				metric = self.hash_set[key + value]
+				metric_args = self.metric_set[metric]
+				dataloader = metric_args['dataloader']
+				if dataloader not in record_dataloader_set:
+					record_dataloader_set[dataloader] = []
+				del metric_args['dataloader']
+				record_dataloader_set[dataloader].append(metric_args)
+		for dataloader, metrics in record_dataloader_set.items():
+			dataloader_args = self.dataloader_set[dataloader]
+			record['dataloader'].append([dataloader_args, metrics])
+		for _, wordvec_args in self.wordvec_set.items():
+			record['wordvec'].append(wordvec_args)
+		return record
 
 	def add_dataloader(self, obj, dataloader, args):
-		id = len(self.dataloader_id)
-		args["id"] = id
-		self.record["dataloader"][dataloader] = args
-		print(obj)
-		self.dataloader_id[obj] = id
-		print(self.dataloader_id)
+		args = compress_dict(args)
+		args['clsname'] = dataloader
+		self.dataloader_set[obj] = args
 
-	def add_standard_metric(self, obj, clsname, metric_type, args):
-		print(obj)
-		print(self.dataloader_id)
-		args['dataloader_id'] = self.dataloader_id[obj]
-		self.record["standard_metric"][clsname + "_" + metric_type] = args
+	def add_metric(self, obj, metric, args):
+		dataloader = args['dataloader']
+		del args['dataloader']
+		args = compress_dict(args)
+		args['clsname'] = metric
+		args['dataloader'] = dataloader
+		self.metric_set[obj] = args
+
+	def invoke_metric_close(self, obj, return_dict):
+		for key, value in return_dict.items():
+			if "hashvalue" in key:
+				self.hash_set[key + value] = obj
 
 	def add_wordvec(self, obj, wordvec, args):
-		self.record["wordvec"][wordvec] = args
+		args = compress_dict(args)
+		args['clsname'] = wordvec
+		self.wordvec_set[obj] = args
 
 def start_recorder():
 	r'''Start recorder'''
@@ -105,8 +164,8 @@ def start_recorder():
 	hooks_listener.clear()
 	hooks_listener.append(SimpleHooksListener())
 
-def close_recorder():
+def close_recorder(result_dict):
 	r'''Close recorder and return the recorded information.'''
 	global hooks_listener
 	assert len(hooks_listener) == 1
-	return hooks_listener[0].close()
+	return hooks_listener[0].close(result_dict)
